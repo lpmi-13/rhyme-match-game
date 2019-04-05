7edd9286f80a21ca1d6311eb46025526f711f9c4
lpmi-13 <leskis@gmail.com>
2019-04-05 19:06:51 +0100

    pass props to my homies

--- tests/Game.test.js
+++ tests/Game.test.js
@@ -5,7 +5,8 @@ import Game from '../src/routes/game';
 
 describe('the Game component', () => {
   it('renders the title', () => {
-    const game = <Game />;
+    const defaultProps = { 'rhyme': {"word": "say", "rhyme": "ay"}}
+    const game = <Game {...defaultProps}/>;
     expect(game).to.contain('header');
   });
 });
