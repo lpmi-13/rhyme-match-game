0de9c1e6eae6964d074ddbac57833b049eb5f4bd
lpmi-13 <leskis@gmail.com>
2019-04-06 08:50:22 +0100

    actually add prettier and update routes for loss

--- src/components/app.js
+++ src/components/app.js
@@ -1,31 +1,30 @@
-import { h, Component } from 'preact';
-import { Router } from 'preact-router';
+import { h, Component } from 'preact'
+import { Router } from 'preact-router'
 
-import Home from '../routes/home';
-import Game from '../routes/game';
-import Loss from '../routes/loss';
-import Select from '../routes/select';
-import Win from '../routes/win';
+import Home from '../routes/home'
+import Game from '../routes/game'
+import Loss from '../routes/loss'
+import Select from '../routes/select'
+import Win from '../routes/win'
 
 export default class App extends Component {
+  state = {
+    selectedRhyme: '',
+  }
 
-	state = {
-		selectedRhyme: '',
-	};
+  selectRhyme = selectedRhyme => this.setState({ selectedRhyme })
 
-    selectRhyme = selectedRhyme => this.setState({ selectedRhyme });
-
-	render() {
-		return (
-			<div id="app">
-				<Router onChange={this.handleRoute}>
-					<Home exact path="/" />
-					<Game path="/game" rhyme={this.state.selectedRhyme} />
-					<Loss path="/loss" />
-					<Select path="/select" onSelectRhyme={this.selectRhyme} />
-					<Win path="/win" />
-				</Router>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="app">
+        <Router onChange={this.handleRoute}>
+          <Home exact path="/" />
+          <Game path="/game" rhyme={this.state.selectedRhyme} />
+          <Loss path="/loss" />
+          <Select path="/select" onSelectRhyme={this.selectRhyme} />
+          <Win path="/win" />
+        </Router>
+      </div>
+    )
+  }
 }
